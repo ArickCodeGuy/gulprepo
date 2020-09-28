04da9ad96b0fc3578c564bb9726ee56c1388d9f3
ArickCodeGuy <arickmail1997@gmail.com>
2020-09-28 08:39:46 +0300

    'fourth'

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
-sass.compiler = require('node-sass');
 var concat = require('gulp-concat');
 var fileinclude = require('gulp-file-include');
+var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
 
 
 gulp.task('sass', function() {
-	return gulp.src('app/scss/**/*.scss')
+	return gulp.src('app/scss/**/style.scss')
 		.pipe(sass().on('error', sass.logError))
-		// concatinating files to one
-		.pipe(concat('style.css'))
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function() {
 	// may be in future it'll be compiled or smth
 	return gulp.src('app/js/**/*.js')
-		.pipe(gulp.dest('dist/js/'));
+		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('html', function() {
@@ -26,13 +25,21 @@ gulp.task('html', function() {
 			prefix: '@@',
 			basepath: '@file'
 		}))
-		.pipe(gulp.dest('dist/'));
+		.pipe(gulp.dest('dist'));
 });
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**')
+		.pipe(changed('dist/img'))
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'));
+})
 
 gulp.task('watch', function(){
 	gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
 	gulp.watch('app/*.html', gulp.series('html'));
 	gulp.watch('app/js/*.js', gulp.series('js'));
+	gulp.watch('app/img/**', gulp.series('imagemin'));
 });
 
 gulp.task('default', gulp.series('sass', 'js', 'html', 'watch'));
