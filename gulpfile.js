66e2c334bea8eac03d0794496b31da0b89811225
ArickCodeGuy <arickmail1997@gmail.com>
2020-10-15 21:46:15 +0300

    Previous commit didn't work correctly

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,13 @@ var fileinclude = require('gulp-file-include');
 var imagemin = require('gulp-imagemin');
 var changed = require('gulp-changed');
 var beautify = require('gulp-beautify');
+// check sourcemaps for working
 var sourcemaps = require('gulp-sourcemaps');
+var webp = require('gulp-webp');
+var webpHtml = require('gulp-webp-html');
 
 
+// compiling sass, beautifying it, moving to dist
 gulp.task('sass', function() {
 	return gulp.src('app/scss/**/*.scss')
 		.pipe(sourcemaps.init())
@@ -18,27 +22,34 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('dist/css'));
 });
 
+// just transfering js file from one folder to another
 gulp.task('js', function() {
 	// may be in future it'll be compiled or smth
 	return gulp.src('app/js/**/*.js')
 		.pipe(gulp.dest('dist/js'));
 });
 
+// compiling html files
 gulp.task('html', function() {
 	return gulp.src('app/**/*.html')
+		.pipe(webpHtml())
 		.pipe(fileinclude({
 			prefix: '@@',
 			basepath: '@file'
 		}))
+		.pipe(beautify.html({intent_size:2}))
 		.pipe(gulp.dest('dist'));
 });
 
+// minifying all images, creating .webp version of it (if it's .png, .jpg, .gif), distributing to dist
 gulp.task('imagemin', function() {
 	return gulp.src('app/img/**')
 		.pipe(changed('dist/img'))
 		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'))
+		.pipe(webp())
 		.pipe(gulp.dest('dist/img'));
-})
+});
 
 gulp.task('watch', function(){
 	gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
