26786578458a9e0b45e80f4bfc1e18803560a43f
unknown <arickmail1997@gmail.com>
2021-01-22 17:31:24 +0300

    small fixes, and changes

--- app/components/form-popup/main.js
+++ app/components/form-popup/main.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function($) {
-	$('.form-popup-toggler').click( function (e) {
-		e.preventDefault();
-		$('.form-popup').slideToggle('fast');
-	})
-	$('.form-popup__close, .form-popup').click( function (e) {
-		if (e.target === e.currentTarget) {
-			$('.form-popup').slideToggle('fast');
-		};
-	});
+  $('.form-popup-toggler').click( function (e) {
+    e.preventDefault();
+    $('.form-popup').slideToggle('fast');
+    document.querySelector('body').classList.add('body-form-toggled');
+  });
+  $('.form-popup__close, .form-popup__body, .form-popup').click( function (e) {
+    if (e.target === e.currentTarget) {
+      $(this).closest('.form-popup').slideToggle('fast');
+      document.querySelector('body').classList.remove('body-form-toggled');
+    };
+  });
 });
