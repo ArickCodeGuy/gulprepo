e45e18976623b9cffa5ca0c8f4e688a7481ad2ec
unknown <arickmail1997@gmail.com>
2021-02-26 15:44:13 +0300

    [FIX] phonemask issue resolved

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-let phoneMaskEl = document.querySelectorAll('.phone-mask');
-for (let el of phoneMaskEl) {
-	IMask(el, {
-		mask: '+{7}(000)000-00-00'
-	});
-};
+window.addEventListener('load', () => {
+  let phoneMaskEl = document.querySelectorAll('.phone-mask');
+  for (let el of phoneMaskEl) {
+  	IMask(el, {
+  		mask: '+{7}(000)000-00-00'
+  	});
+  };
+});
