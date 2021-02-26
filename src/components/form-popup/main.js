27ea61cb2daa11937a794c0296a3d6360284b9ce
unknown <arickmail1997@gmail.com>
2021-02-26 16:10:30 +0300

    [UPDATE] Form popups are now loaded via ajax using fetch

--- src/components/form-popup/main.js
+++ src/components/form-popup/main.js
@@ -1,8 +1,25 @@
 jQuery(document).ready(function($) {
   $('.form-popup-toggler').click( function (e) {
     e.preventDefault();
-    $('.default-form-popup').slideToggle('fast');
-    document.querySelector('body').classList.add('body-form-toggled');
+    let formType = this.dataset.form;
+    if (formType) {
+      fetch(`components/default-form/${formType}.html`)
+        .then((response) => {
+          if (response.status != 200) {
+            alert('couldn\'t find form with given name');
+            throw 'couldn\'t find form with given name';
+          };
+          return response.text();
+        })
+        .then((data) => {
+          $('.default-form-popup').slideToggle('fast');
+          document.querySelector('body').classList.add('body-form-toggled');
+          let formInner = document.getElementById('form-popup__inner');
+          formInner.innerHTML = data;
+        });
+    }else {
+      alert('Data attribute is not set');
+    };
   });
   $('.form-popup__close, .form-popup__body, .form-popup').click( function (e) {
     if (e.target === e.currentTarget) {
