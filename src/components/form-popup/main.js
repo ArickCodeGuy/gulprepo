6411373bb1c1f3ccafd11838ac57d53bd93e71d4
ArickCodeGuy <arickmail1997@gmail.com>
2021-03-03 08:27:26 +0300

    [update] form popups now called via ajax in fancybox

--- src/components/form-popup/main.js
+++ src/components/form-popup/main.js
@@ -1,30 +1,8 @@
 jQuery(document).ready(function($) {
-  $('.form-popup-toggler').click( function (e) {
-    e.preventDefault();
-    let formType = this.dataset.form;
-    if (formType) {
-      fetch(`components/default-form/${formType}.html`)
-        .then((response) => {
-          if (response.status != 200) {
-            alert('couldn\'t find form with given name');
-            throw 'couldn\'t find form with given name';
-          };
-          return response.text();
-        })
-        .then((data) => {
-          $('.default-form-popup').slideToggle('fast');
-          document.querySelector('body').classList.add('body-form-toggled');
-          let formInner = document.getElementById('form-popup__inner');
-          formInner.innerHTML = data;
-        });
-    }else {
-      alert('Data attribute is not set');
-    };
-  });
-  $('.form-popup__close, .form-popup__body, .form-popup').click( function (e) {
-    if (e.target === e.currentTarget) {
-      $(this).closest('.form-popup').slideToggle('fast');
-      document.querySelector('body').classList.remove('body-form-toggled');
-    };
+  $('.form-popup-toggler').fancybox({
+    type: 'ajax',
+    afterLoad: function() {
+      console.log(this.opts.attr);
+    }
   });
 });
