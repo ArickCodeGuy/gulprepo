d5f4f15691061f84898a7ce4659bac9a6e9262b8
unknown <arickmail1997@gmail.com>
2021-04-13 17:49:40 +0300

    lil change for burger event

--- src/components/header/main.js
+++ src/components/header/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
-  $('.mobile-header__burger').click(function(e) {
+  $('.header-menu-toggler').click(function(e) {
     this.classList.toggle('toggled');
-    $('.mobile-header__nav').toggle('fast');
+    $('.header-menu').toggle('fast');
   });
 });
