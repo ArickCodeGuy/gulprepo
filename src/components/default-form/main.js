57ec6d0580a194a346c92e2ad9cd422c63281bce
unknown <arickmail1997@gmail.com>
2021-04-29 17:11:17 +0300

    fancybox popup inline update

--- src/components/default-form/main.js
+++ src/components/default-form/main.js
@@ -1,23 +1,6 @@
 jQuery(document).ready(function($) {
-  $('.form-popup-toggler').click((e) => {
-    e.preventDefault();
-    let popupId = '0';
-    e.currentTarget.dataset.popupId ? popupId = e.currentTarget.dataset.popupId: false;
-    $(`#form-popup-${popupId}`).slideToggle('fast');
-    document.body.classList.toggle('form-popup-toggled');
-  });
-
+  $('.form-poup-opener').fancybox();
   $('.form-popup-closer').click((e) => {
-    $(e.currentTarget).parents('.form-popup').slideUp('fast');
-    document.body.classList.remove('form-popup-toggled');
-  });
-
-  // if clicks outside of .form-popup__content then close popup
-  // never put .form-popup-closer outside of .form-popup__content
-  $('.form-popup').click((e) => {
-    if (e.target.querySelector('.form-popup__content')) {
-      $(e.currentTarget).slideUp('fast');
-      document.body.classList.remove('form-popup-toggled');
-    };
-  });
+    $.fancybox.close();
+  })
 });
